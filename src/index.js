7512f87b5c27ddd8043d8ab931339ca238728514
Regiane Brili <regiane.brili@gmail.com>
2020-07-29 20:40:20 -0300

    Aula 2 - Roteamento e State

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/Video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Menu from '../../components/Menu'
+import dados_iniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
+
+function Home() {
+  return (
+    <div>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dados_iniciais.categorias[0].videos[0].title}
+        url = {dados_iniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        
+      />
+      
+      <Carousel 
+        ignoreFirstVideo
+        category={dados_iniciais.categorias[0]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[1]} />
+      <Carousel category={dados_iniciais.categorias[2]} />
+      <Carousel category={dados_iniciais.categorias[3]} />
+      <Carousel category={dados_iniciais.categorias[4]} />
+      <Carousel category={dados_iniciais.categorias[5]} />
+
+      <Footer />
+
+    </div>
+
+  );
+}
+
+export default Home;
